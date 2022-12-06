cd9ef1f59a03aa8ab0fd2e326b6458e17f1ec1ba
Guilherme Simoes <guilherme.rdems@gmail.com>
2022-12-06 14:53:24 +0000

    Rework

--- main.js
+++ main.js
@@ -3,30 +3,10 @@ import { doWork } from './work.js'
 let alertButton = document.querySelector('.js-alert-button')
 let uiWorkButton = document.querySelector('.js-ui-work-button')
 let workerWorkButton = document.querySelector('.js-worker-work-button')
+let workAmountInput = document.querySelector('.js-work-amount-input')
 
-alertButton.addEventListener('click', showAlert)
-uiWorkButton.addEventListener('click', () => doWork(workAmount, 'UI'))
-workerWorkButton.addEventListener('click', () => worker.postMessage(workAmount))
-
-function showAlert() {
-  alert('You clicked me!')
-}
-
-let workAmount = 1000000000
-
-function evaluateHardwarePerformance() {
-  let start = performance.now()
-  let eventLoopDuration
-
-  setTimeout(() => {
-    let end = performance.now()
-    eventLoopDuration = end - start
-    let eventLoopFactor = Math.max(Math.log10(eventLoopDuration), 1)
-    workAmount = Math.round(200000000 * navigator.hardwareConcurrency / eventLoopFactor)
-  }, 0)
-}
-
-evaluateHardwarePerformance()
+uiWorkButton.addEventListener('click', () => doWork(Number(workAmountInput.value), 'UI'))
+workerWorkButton.addEventListener('click', () => worker.postMessage(Number(workAmountInput.value)))
 
 console.log('UI: creating Worker')
 let worker = new Worker('./worker.js', { type: 'module' }) // classic
@@ -36,3 +16,21 @@ worker.addEventListener('message', async (event) => {
   }
 })
 console.log('UI: now listening to Worker')
+
+function evaluateHardwarePerformance() {
+  let start = performance.now()
+  let eventLoopDuration
+
+  setTimeout(() => {
+    let end = performance.now()
+    eventLoopDuration = end - start
+    let eventLoopFactor = Math.max(Math.log10(eventLoopDuration), 1)
+    let workAmount = 200000000 * navigator.hardwareConcurrency / eventLoopFactor
+
+    console.log(workAmount);
+    workAmountInput.value = workAmount
+    workAmountInput.step = workAmount / 10
+  }, 0)
+}
+
+evaluateHardwarePerformance()
